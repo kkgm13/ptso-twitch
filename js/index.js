14d611406563746633fea6ff7128059a9c23f290
Kel Ming <kkgm013@gmail.com>
2022-06-01 14:21:02 +0100

    Skeleton Update for more streamline edition

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-$().get('https://www.twitch.tv/'+ 'KKGM13', function(data) {
-    var imgs = $("figure[aria-label=" + "'KKGM13'"+"]").html();
-    imgs.each(function(i, img) {
-        alert(img.src); // show a dialog containing the url of image
-    });
+var streamer = "KKGM13";
+
+$().get('https://www.twitch.tv/'+ streamer , function(data) {
+    var imgs = $("figure[aria-label=" + streamer +"]").html().find("figure[aria-label='"+streamer+"']");
+    // imgs.each(function(i, img) {
+    //     alert(img.src); // show a dialog containing the url of image
+    // });
 });
