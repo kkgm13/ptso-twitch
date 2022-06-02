ee527b208941cc7543c9b039b3e349a403c56dcf
Kel Ming <KKGM013@gmail.com>
2022-06-02 13:38:53 +0100

    fixes and web design changes

--- admin/js/streamer-table.js
+++ admin/js/streamer-table.js
@@ -15,9 +15,7 @@ export default {
             console.log(item)
         },
         loadData(){
-            json.forEach(x => {
-                console.log(x)
-            });
+            this.items = json
         }
     },
     data() {
@@ -37,11 +35,12 @@ export default {
                 }
             ], 
             // Requires a full local File I/O
-            items: [
-                { id: 1, streamerName:'kkgm13', streamerDetails: 'Test1;Test2:test3;test4'},
-                { id: 2, streamerName:'farahino', streamerDetails: 'Test1;Test2:test3;test4'},
-                { id: 3, streamerName:'diamondzbeard',streamerDetails: 'Test1;Test2:test3;test4'},
-            ]
+            items: null
+            // [
+                // { id: 1, streamerName:'kkgm13', streamerDetails: 'Test1;Test2:test3;test4'},
+                // { id: 2, streamerName:'farahino', streamerDetails: 'Test1;Test2:test3;test4'},
+                // { id: 3, streamerName:'diamondzbeard',streamerDetails: 'Test1;Test2:test3;test4'},
+            // ]
         }
     }
 }
