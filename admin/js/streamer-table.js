00d4d8a772638543f93083e9b39ced3239ab0793
Kel Ming <KKGM013@gmail.com>
2022-06-02 17:51:06 +0100

    Changes with admin

--- admin/js/streamer-table.js
+++ admin/js/streamer-table.js
@@ -1,14 +1,16 @@
-import json from '../testData.json' assert { type: "json" };
+// import json from '../testData.json' assert { type: "json" };
 import json2 from '../streamerData.json' assert { type: "json" };
 
 export default {
     mounted(){
-        this.loadData()
         console.log("Table Data mounted successfully")
+    },
+    created(){
+        this.loadData()
     },
     methods: {
         edit(item) {
-            document.getElementById('streamerInput').value = item.streamerName;
+            document.getElementById('streamerName').value = item.streamerName;
             document.getElementById('streamerDetails').value = item.streamerDetails;
         },
         deleteStreamer(item){
@@ -16,22 +18,19 @@ export default {
             console.log(item)
         },
         loadData(){
-            // this.items = json2;
-            this.items = json;
+            this.items = json2;
+            // this.items = json;
         },
         saveData() {
-            console.log(this.items);
-            alert('Derp, im supposed to work');
-            localStorage.setItem()
-
-            // function download(content, fileName, contentType) {
-            //     var a = document.createElement("a");
-            //     var file = new Blob([content], {type: contentType});
-            //     a.href = URL.createObjectURL(file);
-            //     a.download = fileName;
-            //     a.click();
-            // }
-            // download(jsonData, 'json.txt', 'text/plain');
+            function download(content, fileName, contentType) {
+                var a = document.createElement("a");
+                var file = new Blob([content], {type: contentType});
+                a.href = URL.createObjectURL(file);
+                a.download = fileName;
+                a.click();
+            }
+            // Fix Absolute path to keep file synergy
+            download(JSON.stringify(this.items, null, 2), 'streamerData.json', 'application/json');
         }
     },
     data() {
@@ -52,11 +51,6 @@ export default {
             ], 
             // Requires a full local File I/O
             items: null
-            // [
-                // { id: 1, streamerName:'kkgm13', streamerDetails: 'Test1;Test2:test3;test4'},
-                // { id: 2, streamerName:'farahino', streamerDetails: 'Test1;Test2:test3;test4'},
-                // { id: 3, streamerName:'diamondzbeard',streamerDetails: 'Test1;Test2:test3;test4'},
-            // ]
         }
     }
 }
