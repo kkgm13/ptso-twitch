0d232a6a7d247f84b5cac6817be1dec95ad574db
Kel Ming <kkgm013@gmail.com>
2023-08-26 21:49:58 +0100

    Updated Twitch API Access Token collection

---
+++ New/admin/assets/js/twitch-getinfo.js
@@ -0,0 +1,98 @@
+// const path = require('path')
+
+// const dotenvPath = path.join(__dirname, '../../../.env')
+// require('dotenv').config({path: dotenvPath})
+// const fs = require('fs')
+const axios = require('axios').default
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let streamerUserName = null
+let clientid = null
+let clientsecret = null
+let access_token = null
+
+readline.question('Your Twitch Username? ', (twitchUser) => {
+    console.log(`You entered: ${twitchUser}`);
+    streamerUserName = twitchUser
+
+    readline.question('Add Your Twitch Client ID: ', (clientID) => {
+        // console.log(`You entered: ${clientID}`);
+        clientid = clientID
+
+        readline.question('Add Your Twitch Client Secret: ', (clientSecret) => {
+            // console.log(`You entered: ${clientSecret}`);
+            clientsecret = clientSecret            
+            setup()
+            readline.close();
+        });
+    });
+    
+});
+
+function setup(){
+    axios.post('https://id.twitch.tv/oauth2/token', {
+        client_id: clientid,
+        client_secret: clientsecret,
+        grant_type: 'client_credentials'
+    })
+    .then(function (response) {
+        // setToDotEnv('TWITCH_ACCESS_TOKEN', response.data.access_token)
+        access_token = response.data.access_token
+    
+        axios.get('https://api.twitch.tv/helix/users', {
+            params: {
+                login: streamerUserName
+            },
+            headers: {
+                'Authorization': `Bearer ${access_token}`,
+                'Client-Id': clientid
+            }
+        })
+        .then(function (response) {      
+            console.log("")  
+            console.log("--------------------------")
+            console.warn("Parameters for Twitch User: " + streamerUserName + ` (Twitch ID: ${response.data.data[0].id})`)
+            console.log("--------------------------")
+            
+            console.warn("Note: Please immediate populate assets/js/vue/new-streamer.js with the information below")
+            console.log("Twitch Client Access Token: "+access_token)
+        })
+        .catch(function (error) {
+            console.log("ERROR - " + error);
+        });
+    })
+    .catch(function(error) {
+        console.log("Error! - " + error)
+    });
+}
+ 
+
+function setToDotEnv( keyName, valueName){
+    // Read the content of the dotenv file
+    const content = fs.readFileSync(dotenvPath, 'utf-8');
+
+    // Parse the content into key-value pairs
+    const keyValuePairs = content.split('\n').map(line => {
+        const [key, value] = line.split('=');
+        return { key, value };
+    });
+
+    // Find and update the value for the specified key
+    const updatedKeyValuePairs = keyValuePairs.map(item => {
+        if (item.key === keyName) {
+            item.value = valueName;
+        }
+        return item;
+    });
+
+    // Build the updated content
+    const updatedContent = updatedKeyValuePairs.map(item => `${item.key}=${item.value}`).join('\n');
+
+    // Write the updated content back to the dotenv file
+    fs.writeFileSync(dotenvPath, updatedContent);
+
+    console.log(keyName + ' updated successfully.');
+}
--- New/readme.md
+++ New/readme.md
@@ -21,14 +21,12 @@
 - [Santatize-HTMl] (TBC)
 
 ## First Time Running
-0) Login to [Twitch Dev](https://dev.twitch.tv) and get your Client ID after creating your own Localhost Application
-1) Save a copy of the .env.example as .env, and add your Twitch Username & ClientID  (DO NOT DELETE UNDER ANY CIRCUMSTANCE for stable usage) 
-2) After running ```npm update``` on a Command Prompt/Terminal to start up dependencies, run ```npm run setup-env <Your Twitch UserName>``` to setup the newly created dotenv (.env) file with the extra related information
-    - *Note: There maybe a scenario that you will need to rerun```npm run setup-env``` without the extra username argument, now and then if not working correctly.*
+0) Login to [Twitch Dev](https://dev.twitch.tv) and get your Client ID & Client Secret after creating your own Localhost Application
+1) After running ```npm update``` on a Command Prompt/Terminal to start up dependencies, run ```npm run setup``` to get your information and Access Token. Some of these will need your specific Register App details via Twitch Dev
+    - *Note: There maybe a scenario that you will need to rerun```npm run setup``` now and then if not working correctly.*
 
 ## Notes 📝
-1) Use the .env.example to provide your personal information **AND SAVE A COPY** as an ".env" file. 
-2) Please ensure to clear OBS Browser cache **before and after** stream for safety, especially with extensive data work with Admin Section
+1) Please ensure to clear OBS Browser cache **before and after** stream for safety, especially with extensive data work with Admin Section
 3) To have unique lines for a streamer, load up the server and checkout the Admin page on a browser to add unique statements about them.
 
 ## 🛠️ Future Updates to Fix
@@ -36,7 +34,7 @@
 |------|-------|
 |SO.html: Single Line Carousel during activation| **CRITICAL FEATURE** | 
 |📌 Admin: Twitch API Integration |In Progress|
-|⭐️ System: Use Twitch ID instead as a reference system due to potential username changes over time|In Progress|
+|⭐️ System: Use Twitch ID instead as a reference system due to potential username changes over time|Final Testings|
 |Admin: Automatic Front-end Saving|Mid|
 |Admin: Proper Database Calling via MongoDB/MySQL/SQLite|Mid|
 |Admin?: User Customization under request of some Twitch Streamers (eg: Box Color, Font Family, etc...)|In progress|
