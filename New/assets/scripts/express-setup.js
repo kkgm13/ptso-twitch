21bb428a17a3d21c0a6a36678f998b2aeb587b38
Kel Ming <kkgm013@gmail.com>
2023-11-05 20:55:45 +0000

    Updating and starting self-containing system with ExpressJS

--- New/assets/scripts/express-setup.js
+++ New/assets/scripts/express-setup.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const hostname = "127.0.0.1";
 const port = 3000
 const fs = require('fs')
 const path = require('path')
-const dotenvPath = path.join(__dirname, '../../../.env')
-require('dotenv').config({path: dotenvPath})
+// const dotenvPath = path.join(__dirname, '../../../.env')
+// require('dotenv').config({path: dotenvPath})
 
-const sqlite3 = require('sqlite3').verbose()
+// const sqlite3 = require('sqlite3').verbose()
 // const db = sqlite3.Database('streamer.db')
 
 
@@ -28,13 +29,14 @@ function dbCreation(){
     })
 }
 
-app.get('/', (req, res) => {    
-    res.send('Hello World!')
-})
+app.use('/', express.static(path.join(__dirname, '../../admin')))
+
+// app.get('/', (req, res) => {    
+    // res.sendFile(path.join(__dirname, '../../admin/assets/css/stylesheets.css'))
+    // res.sendFile(path.join(__dirname, '../../admin/')) // Works but Vue & csS are broken
+// })
 
 app.listen(port, () => {
     console.log('Accessing .env files')
-
-    console.log(`PTSO-Twitch is active on localhost, listening on port ${port}`)
-    
+    console.log(`PTSO-Twitch is active on localhost, listening on ${hostname}:${port}`)
 })
